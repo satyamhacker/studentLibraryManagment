a63cf37084642a9e3a96e660a0455a86e85b44cb
satyamhacker <satyamgrandmaster@gmail.com>
2025-08-16 19:31:02 +0530

    refactor(backEnd): move routes to separate file
    - Removed route definitions from server.js
    - Created new file index.routes.js in src/api/v1/Routes/
    - Moved all route-related imports and definitions to the new file
    - This change prepares for future implementation of admin and user routes

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,19 +1,5 @@
 import express from "express"; // Import Express
 import cors from "cors"; // Import CORS middleware
-import {
-    signupCreate,
-    login,
-    addStudentData,
-    fetchStudentsData,
-    deleteStudentData,
-    updateStudentData,
-    updatePaymentExpectedDate,
-    exportStudentDataToExcel, // Import the new controller
-    sendOtp, verifyOtp, resetPassword,
-    filterStudentData // Import the filterStudentData controller
-} from "./index.Controller.mjs"; // Import your signupLogin controller
-
-import { VerifyUserJwt } from '../Middleware/Jwt.mjs'; // Import the VerifyUserJwt middleware
 
 import dotenv from 'dotenv';
 
@@ -30,26 +16,7 @@ app.use(express.json());
 
 app.use("/api/v1", mainRoutes);
 
-app.post("/signup", signupCreate); // Route for signup
-app.post("/login", login); // Route for login
-app.post('/sendOtp', sendOtp);
-app.post('/verifyOtp', verifyOtp);
-app.post('/resetPassword', resetPassword);
 
-app.post("/addStudent", VerifyUserJwt, addStudentData);
-
-// Route for fetching student data
-app.get("/getStudents", VerifyUserJwt, fetchStudentsData);
-
-app.delete("/deleteStudent/:id", VerifyUserJwt, deleteStudentData);
-
-app.put("/updateStudent/:id", VerifyUserJwt, updateStudentData);
-
-app.put("/updatePaymentExpectedDate/:id", VerifyUserJwt, updatePaymentExpectedDate);
-
-app.get("/exportStudents", VerifyUserJwt, exportStudentDataToExcel); // Add the new route
-
-app.post("/filterStudentData", VerifyUserJwt, filterStudentData); // Add the new route for filtering student data
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
