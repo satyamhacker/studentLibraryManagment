e646f23f0f23e1e220477e13f17c7ceb669a1b16
satyamhacker <satyamgrandmaster@gmail.com>
2025-08-17 15:20:44 +0530

    fix(controller): rename index.Controller.mjs to index.controller.js
    - Change the file name from index.Controller.mjs to index.controller.js
    - Update the import statements to use .js extension instead of .mjs
    - This change ensures consistency in file naming conventions and improves compatibility with JavaScript environments

---
+++ backEnd/src/api/v1/Controller/index.controller.js
@@ -0,0 +1,26 @@
+import { signupCreate, login } from "./Signup_Login.mjs";
+import { addStudentData } from "./Add_student_data.mjs";
+import { fetchStudentsData } from "./Fetch_student_datas.mjs";
+import { deleteStudentData } from "./Delete_student_data.mjs";
+import { updateStudentData } from "./Update_student_data.mjs";
+import { exportStudentDataToExcel } from "./Export_student_data.mjs";
+import { updatePaymentExpectedDate } from "./Update_payment_expected_date.mjs";
+
+import { sendOtp, verifyOtp, resetPassword } from './Reset_password_related.mjs';
+import {filterStudentData} from './Filter_student_data.mjs';
+
+
+export {
+  signupCreate,
+  login,
+  addStudentData,
+  fetchStudentsData,
+  deleteStudentData,
+  updateStudentData,
+  updatePaymentExpectedDate,
+  exportStudentDataToExcel,
+  sendOtp, 
+  verifyOtp,
+  resetPassword,
+  filterStudentData
+};
