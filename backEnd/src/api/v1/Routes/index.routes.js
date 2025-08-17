dab335527cc79b01db9a142968df507752c903c2
satyamhacker <satyamgrandmaster@gmail.com>
2025-08-17 15:19:30 +0530

    refactor(backEnd): move auth routes to separate file
    - Extracted auth-related routes from index.routes.js to auth.routes.js
    - Commented out all routes and imports for future implementation
    - Maintained the existing structure and naming conventions

--- backEnd/src/api/v1/Routes/index.routes.js
+++ backEnd/src/api/v1/Routes/index.routes.js
@@ -9,38 +9,3 @@ app.use("/auth", authRoutes);
 export default app;
 
 
-// import {
-//     signupCreate,
-//     login,
-//     addStudentData,
-//     fetchStudentsData,
-//     deleteStudentData,
-//     updateStudentData,
-//     updatePaymentExpectedDate,
-//     exportStudentDataToExcel, // Import the new controller
-//     sendOtp, verifyOtp, resetPassword,
-//     filterStudentData // Import the filterStudentData controller
-// } from "./index.Controller.mjs"; // Import your signupLogin controller
-
-// import { VerifyUserJwt } from '../Middleware/Jwt.mjs'; // Import the VerifyUserJwt middleware
-
-// app.post("/signup", signupCreate); // Route for signup
-// app.post("/login", login); // Route for login
-// app.post('/sendOtp', sendOtp);
-// app.post('/verifyOtp', verifyOtp);
-// app.post('/resetPassword', resetPassword);
-
-// app.post("/addStudent", VerifyUserJwt, addStudentData);
-
-// // Route for fetching student data
-// app.get("/getStudents", VerifyUserJwt, fetchStudentsData);
-
-// app.delete("/deleteStudent/:id", VerifyUserJwt, deleteStudentData);
-
-// app.put("/updateStudent/:id", VerifyUserJwt, updateStudentData);
-
-// app.put("/updatePaymentExpectedDate/:id", VerifyUserJwt, updatePaymentExpectedDate);
-
-// app.get("/exportStudents", VerifyUserJwt, exportStudentDataToExcel); // Add the new route
-
-// app.post("/filterStudentData", VerifyUserJwt, filterStudentData); // Add the new route for filtering student data
