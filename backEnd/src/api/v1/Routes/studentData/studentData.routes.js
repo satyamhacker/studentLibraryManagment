a3ccb055baffa6705f1c4c5ff4ac5449ead6536e
satyamhacker <satyamgrandmaster@gmail.com>
2025-08-17 20:53:04 +0530

    feat(studentData): add route for updating student data
    - Import updateStudentData controller
    - Add PATCH route for updating student data
    - Update existing routes for consistency

--- backEnd/src/api/v1/Routes/studentData/studentData.routes.js
+++ backEnd/src/api/v1/Routes/studentData/studentData.routes.js
@@ -6,12 +6,13 @@ import { addStudentData, deleteStudentData, fetchAllStudentData } from "../../Co
 import validator from "../../Middleware/validators/validators.middleware.js";
 import { validators } from "../../Validators/index.validators.js";
 import { VerifyUserJwt } from "../../Middleware/jwt.middleware.js";
+import { updateStudentData } from "../../Controller/updateStudentData.controller.js";
 
 const router = express.Router();
 
 router.post("/add-student-data", validator(validators.studentData.addStudentDataValidator), VerifyUserJwt, addStudentData); // Route for signup
 router.delete("/delete-student-data/:id", VerifyUserJwt, deleteStudentData); // Route for login
 router.get("/fetch-all-student-data", VerifyUserJwt, fetchAllStudentData);
-
+router.patch("/update-student-data/:id", VerifyUserJwt, updateStudentData); // Route for login
 
 export default router;
