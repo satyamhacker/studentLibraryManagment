dba4d7deab652261f45ade32c171367f5e8fead3
satyamhacker <satyamgrandmaster@gmail.com>
2025-08-21 18:13:21 +0530

    refactor(frontEnd): remove ContextData wrapper from main component
    - Remove ContextData component from the root rendering
    - Simplify the structure of the main.jsx file
    - Improve performance by eliminating unnecessary context wrapping

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/tailwind.css";
 
-import ContextData from "./Context/contextData.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ContextData>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ContextData>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+
 );
